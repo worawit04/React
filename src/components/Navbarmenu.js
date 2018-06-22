fa009b88d68a70ccef5b3398b3ee09edebaea131
YOURNAME <worawit@hotpital-os.com>
2018-06-22 15:43:07 +0700

    final1

--- src/components/Navbarmenu.js
+++ src/components/Navbarmenu.js
@@ -16,8 +16,8 @@ class Navbarmenu extends Component{
             return(
                 <div className="flex-grow pa3 flex items-center">
                 <a className="f6 link dib white dim mr3 mr4-ns" href="/">Home</a>
-                <a className="f6 link dib white dim mr3 mr4-ns" href="/dashboard">Dashboard</a>
                 <a className="f6 link dib white dim mr3 mr4-ns" href="/addnews">Addnews</a>
+                <a className="f6 link dib white dim mr3 mr4-ns" href="/dashboard">Dashboard</a>
                 <a className="f6 dib white bg-animate hover-bg-white hover-black no-underline pv2 ph4 br-pill ba b--white-20" onClick={this.isLogout}>Logout</a>
                 
             </div>
